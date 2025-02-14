0eaccaa9e73aece2ca90f28ac29955920a33ee7d
Andrew de Souza ribeiro <andrewsouza57@gmail.com>
2025-02-13 22:47:06 -0300

    fix: finalization project, integration mongoDb created api and correctly front

---
+++ back-end/api/connect.js
@@ -0,0 +1,7 @@
+import { MongoClient } from "mongodb"
+
+const URI = "mongodb+srv://andrewsouza57:G8HSlyK5kcsmBzFw@cluster0.1olql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(URI);
+
+export const db = client.db("spotify");

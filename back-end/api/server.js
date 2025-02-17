7d34cc6effa1bd693beb4ab084d1ed0b4bfabd8b
Andrew Souza <andrew.souza@aigroup.local>
2025-02-17 11:54:46 -0300

    fix:deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -22,10 +22,10 @@ app.get("/api/songs", async (req, res) => {
   res.send(await db.collection("songs").find({}).toArray());
 });
 
-app.use(express.static(path.join(__dirname, "../public/dist")));
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
 
 app.get("*", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
